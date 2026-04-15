b5dfa12291f7e6f8e9c5ee67e599071ad7681402
daguilar3212 <daguilar@vehiculosdecr.com>
2026-04-14 21:32:36 -0600

    Delete fetch-gmail-invoices.js

--- fetch-gmail-invoices.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  const clientId = process.env.GMAIL_CLIENT_ID;
-  const redirectUri = process.env.GMAIL_REDIRECT_URI;
-  const scope = encodeURIComponent('https://www.googleapis.com/auth/gmail.readonly');
-  const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${scope}&access_type=offline&prompt=consent`;
-  res.redirect(302, url);
-}
