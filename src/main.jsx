42d85198953d0ae44e07d59a53a4f42ad5a7e473
daguilar3212 <daguilar@vehiculosdecr.com>
2026-04-16 15:40:24 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import AppGuard from './AppGuard.jsx'
 import { AuthProvider } from './AuthProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <AppGuard />
     </AuthProvider>
   </React.StrictMode>,
 )
