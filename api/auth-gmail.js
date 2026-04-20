33baea5244dc49469f7833f62a1d4bbf7a9aaa02
daguilar3212 <daguilar@vehiculosdecr.com>
2026-04-19 21:41:01 -0600

    Update auth-gmail.js

--- api/auth-gmail.js
+++ api/auth-gmail.js
@@ -1,7 +1,8 @@
 export default function handler(req, res) {
   const clientId = process.env.GMAIL_CLIENT_ID;
   const redirectUri = process.env.GMAIL_REDIRECT_URI;
-  const scope = encodeURIComponent('https://www.googleapis.com/auth/gmail.readonly');
+  // Scopes: Gmail lectura + Sheets escritura
+  const scope = encodeURIComponent('https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/spreadsheets');
   const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${scope}&access_type=offline&prompt=consent`;
   res.redirect(302, url);
 }
